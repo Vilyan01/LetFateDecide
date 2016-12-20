70701f0dc0e4f25ed56434035f4f01d87e353fdb
Brian Heller <bheller2010@live.com>
2016-12-19 23:55:27 -0800

    Choice group lives in model folder where it belongs.

---
+++ LetFateDecide/models/ChoiceGroup.swift
@@ -0,0 +1,51 @@
+//
+//  ChoiceGroup.swift
+//  LetFateDecide
+//
+//  Created by Brian Heller on 12/19/16.
+//  Copyright © 2016 Brian Heller. All rights reserved.
+//
+
+import Foundation
+
+/*
+ Pretty much just an array of choice objects, but we want to perform some basic operations on them.
+ */
+
+struct ChoiceGroup {
+    private var choices = [Choice]()
+    
+    // Add a choice to the group.
+    mutating func addChoiceToGroup(choice:Choice) {
+        choices.append(choice)
+    }
+    
+    // 'select' a choice at an index
+    mutating func selectChoiceAtIndex(index:NSInteger) {
+        if(index >= 0 && index < choices.count) {
+            choices[index].decided = true
+        }
+    }
+    
+    // clear all choices
+    mutating func clearAllChoices() {
+        choices.removeAll()
+    }
+    
+    // reset the choices back to default
+    mutating func resetChoices() {
+        for i in 0...choices.count - 1 {
+            choices[i].decided = false
+        }
+    }
+    
+    // returns the number of choices in the array.
+    func numberOfChoices() -> Int {
+        return choices.count
+    }
+    
+    // gets a choice for a given index path
+    func choiceForIndexPath(indexPath:IndexPath) -> Choice {
+        return choices[indexPath.row];
+    }
+}
